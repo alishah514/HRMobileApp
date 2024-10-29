a0f05a8bf779d0e0a5626546589ab24c9c516528
Syed Ali Sultan Bukhari <syedalisultanbukhari5@gmail.com>
2024-10-29 18:11:23 +0500

    Patch Update profile through redux api implemented

--- src/services/api/profile/ProfileService.js
+++ src/services/api/profile/ProfileService.js
@@ -54,6 +54,107 @@ const ProfileService = {
       throw error;
     }
   },
+
+  patchEditProfile: async (profileId, profileData) => {
+    const url = `${Constants.FIREBASE_URL}/${Constants.EMPLOYEES}/${profileId}?key=${Constants.FIREBASE_KEY}`;
+    const method = 'patch';
+
+    const body = {
+      fields: {
+        job: {
+          mapValue: {
+            fields: {
+              Designation: {
+                stringValue: profileData.job?.Designation || '',
+              },
+              Department: {
+                stringValue: profileData.job?.Department || '',
+              },
+              JoiningDate: {
+                timestampValue: profileData.job?.JoiningDate || '',
+              },
+              employmentType: {
+                stringValue: profileData.job?.employmentType || '',
+              },
+              salary: {
+                stringValue: profileData.job?.salary || '',
+              },
+              wageType: {
+                stringValue: profileData.job?.wageType || '',
+              },
+            },
+          },
+        },
+        personal: {
+          mapValue: {
+            fields: {
+              employeeId: {
+                stringValue: profileId,
+              },
+              fullName: {
+                stringValue: profileData.personal?.fullName || '',
+              },
+              phone: {
+                integerValue: profileData.personal?.phone || '0',
+              },
+              email: {
+                stringValue: profileData.personal?.email || '',
+              },
+              birthDate: {
+                timestampValue: profileData.personal?.birthDate || '',
+              },
+              gender: {
+                stringValue: profileData.personal?.gender || '',
+              },
+            },
+          },
+        },
+        userId: {
+          stringValue: profileData.userId || '',
+        },
+        education: {
+          arrayValue: {
+            values:
+              profileData.education?.map(edu => ({
+                mapValue: {
+                  fields: {
+                    startYear: {
+                      integerValue: edu.startYear || '0',
+                    },
+                    startDate: {
+                      timestampValue: edu.startDate || '',
+                    },
+                    Institute: {
+                      stringValue: edu.Institute || '',
+                    },
+                    Degree: {
+                      stringValue: edu.Degree || '',
+                    },
+                    endYear: {
+                      integerValue: edu.endYear || '0',
+                    },
+                    endDate: {
+                      timestampValue: edu.endDate || '',
+                    },
+                  },
+                },
+              })) || [],
+          },
+        },
+      },
+    };
+
+    try {
+      const response = await axios.patch(url, body);
+      return {success: true, response: response.data};
+    } catch (error) {
+      console.error('Error in LeaveService.patchLeaveStatus:', error);
+      return {
+        success: false,
+        error: error.message || 'An unexpected error occurred.',
+      };
+    }
+  },
 };
 
 export default ProfileService;
