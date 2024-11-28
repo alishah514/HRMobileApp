175741260cf83b8510e583cf7d21cddb094d14ea
Syed Ali Sultan Bukhari <syedalisultanbukhari5@gmail.com>
2024-11-28 11:53:38 +0500

    THREE FIELDS IN TIM EOUT TIME AND TOTAL HOURS ADDED ON PROFILE

--- src/screens/profile/types/WorkInfo.js
+++ src/screens/profile/types/WorkInfo.js
@@ -19,6 +19,44 @@ export default function WorkInfo({data}) {
     );
   }
 
+  const formatTime = time => {
+    const numericTime = Number(time);
+    if (isNaN(numericTime)) {
+      console.log('Invalid time:', time);
+      return 'null';
+    }
+
+    const isAM = numericTime < 12;
+    const formattedHours = isAM
+      ? numericTime === 0
+        ? 12
+        : numericTime
+      : numericTime - 12 || 12;
+    const period = isAM ? 'AM' : 'PM';
+
+    const formattedTime = `${formattedHours}:00 ${period}`;
+
+    return formattedTime;
+  };
+
+  const calculateWorkDuration = (punchIn, punchOut) => {
+    punchIn = Number(punchIn);
+    punchOut = Number(punchOut);
+
+    if (isNaN(punchIn) || isNaN(punchOut)) {
+      console.error('Invalid punchIn or punchOut values:', punchIn, punchOut);
+      return 'null';
+    }
+
+    const duration = punchOut - punchIn;
+    const hours = Math.floor(duration);
+    const minutes = Math.round((duration - hours) * 60);
+
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')} Hours`;
+  };
+
   return (
     <View>
       <Text
@@ -169,6 +207,80 @@ export default function WorkInfo({data}) {
                 CommonStyles.paddingRight1,
               ]}>
               {TruncateTitle(data?.wageType || 'null', 20)}
+            </Text>
+          </View>
+        </View>
+        <View style={CommonStyles.borderLineWithoutMargin} />
+      </View>
+      <View style={CommonStyles.paddingTop5}>
+        <View style={CommonStyles.rowBetween}>
+          <Text
+            style={[
+              CommonStyles.font5,
+              CommonStyles.textBlack,
+              CommonStyles.lessBold300,
+            ]}>
+            {I18n.t('punchInTime')}
+          </Text>
+          <View style={[CommonStyles.flexRow, CommonStyles.centerView]}>
+            <Text
+              style={[
+                CommonStyles.font5,
+                CommonStyles.textBlack,
+                CommonStyles.Bold600,
+                CommonStyles.paddingRight1,
+              ]}>
+              {data?.punchInTime ? formatTime(data.punchInTime) : 'null'}
+            </Text>
+          </View>
+        </View>
+        <View style={CommonStyles.borderLineWithoutMargin} />
+      </View>
+      <View style={CommonStyles.paddingTop5}>
+        <View style={CommonStyles.rowBetween}>
+          <Text
+            style={[
+              CommonStyles.font5,
+              CommonStyles.textBlack,
+              CommonStyles.lessBold300,
+            ]}>
+            {I18n.t('punchOutTime')}
+          </Text>
+          <View style={[CommonStyles.flexRow, CommonStyles.centerView]}>
+            <Text
+              style={[
+                CommonStyles.font5,
+                CommonStyles.textBlack,
+                CommonStyles.Bold600,
+                CommonStyles.paddingRight1,
+              ]}>
+              {data?.punchOutTime ? formatTime(data.punchOutTime) : 'null'}
+            </Text>
+          </View>
+        </View>
+        <View style={CommonStyles.borderLineWithoutMargin} />
+      </View>
+      <View style={CommonStyles.paddingTop5}>
+        <View style={CommonStyles.rowBetween}>
+          <Text
+            style={[
+              CommonStyles.font5,
+              CommonStyles.textBlack,
+              CommonStyles.lessBold300,
+            ]}>
+            {I18n.t('totalWorkHours')}
+          </Text>
+          <View style={[CommonStyles.flexRow, CommonStyles.centerView]}>
+            <Text
+              style={[
+                CommonStyles.font5,
+                CommonStyles.textBlack,
+                CommonStyles.Bold600,
+                CommonStyles.paddingRight1,
+              ]}>
+              {data?.punchInTime && data?.punchOutTime
+                ? calculateWorkDuration(data.punchInTime, data.punchOutTime)
+                : 'null'}
             </Text>
           </View>
         </View>
