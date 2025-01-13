93fabcfc8e8bacc9292523049c6608dcb970e0a0
Syed Ali Sultan Bukhari <syedalisultanbukhari5@gmail.com>
2025-01-13 17:17:08 +0500

    announcements generic api component implemented

--- src/services/api/announcements/AnnouncementsService.js
+++ src/services/api/announcements/AnnouncementsService.js
@@ -1,13 +1,14 @@
 import Constants from '../../../components/common/Constants';
-import AnnouncementsApiComponent from './AnnouncementsApiComponent';
+import GenericApiComponent from '../GenericApiComponent';
 
 const AnnouncementsService = {
   fetchAnnouncements: async () => {
     const url = `${Constants.FIREBASE_URL}/${Constants.ANNOUNCEMENTS}?key=${Constants.FIREBASE_KEY}`;
     const method = 'get';
+    const options = {resourceType: 'Announcement'};
 
     try {
-      const response = await AnnouncementsApiComponent(url, method);
+      const response = await GenericApiComponent(url, method, null, options);
       return response;
     } catch (error) {
       throw error;
@@ -17,33 +18,26 @@ const AnnouncementsService = {
   postAnnouncement: async data => {
     const url = `${Constants.FIREBASE_URL}/${Constants.ANNOUNCEMENTS}?key=${Constants.FIREBASE_KEY}`;
     const method = 'post';
+    const options = {
+      resourceType: 'Announcement',
+      postFlag: true,
+    };
+
     const body = {
       fields: {
-        title: {
-          stringValue: data.title,
-        },
-        content: {
-          stringValue: data.message,
-        },
-        creationDate: {
-          timestampValue: data.creationDate,
-        },
-        adminId: {
-          stringValue: data.adminId,
-        },
-
-        attachment: {
-          stringValue: data.attachment,
-        },
+        title: {stringValue: data.title},
+        content: {stringValue: data.message},
+        creationDate: {timestampValue: data.creationDate},
+        adminId: {stringValue: data.adminId},
+        attachment: {stringValue: data.attachment},
       },
     };
 
     try {
-      const response = await AnnouncementsApiComponent(url, method, body, true);
-
+      const response = await GenericApiComponent(url, method, body, options);
       return {success: true, response};
     } catch (error) {
-      console.error('Error in TaskService.postTask:', error);
+      console.error('Error in AnnouncementsService.postAnnouncement:', error);
       return {
         success: false,
         error: error.message || 'An unexpected error occurred.',
@@ -52,36 +46,27 @@ const AnnouncementsService = {
   },
 
   patchAnnouncement: async (announcementId, data) => {
-    console.log('announcementId, data', announcementId, data);
-
     const url = `${Constants.FIREBASE_URL}/${Constants.ANNOUNCEMENTS}/${announcementId}?key=${Constants.FIREBASE_KEY}`;
     const method = 'patch';
+    const options = {
+      resourceType: 'Announcement',
+    };
 
     const body = {
       fields: {
-        title: {
-          stringValue: data.title,
-        },
-        content: {
-          stringValue: data.message,
-        },
-        creationDate: {
-          timestampValue: data.creationDate,
-        },
-        adminId: {
-          stringValue: data.adminId,
-        },
-        attachment: {
-          stringValue: data.attachment,
-        },
+        title: {stringValue: data.title},
+        content: {stringValue: data.message},
+        creationDate: {timestampValue: data.creationDate},
+        adminId: {stringValue: data.adminId},
+        attachment: {stringValue: data.attachment},
       },
     };
 
     try {
-      const response = await AnnouncementsApiComponent(url, method, body);
+      const response = await GenericApiComponent(url, method, body, options);
       return {success: true, response};
     } catch (error) {
-      console.error('Error in Announcement Service.patchAnnouncement:', error);
+      console.error('Error in AnnouncementsService.patchAnnouncement:', error);
       return {
         success: false,
         error: error.message || 'An unexpected error occurred.',
@@ -91,10 +76,13 @@ const AnnouncementsService = {
 
   deleteAnnouncement: async announcementId => {
     const url = `${Constants.FIREBASE_URL}/${Constants.ANNOUNCEMENTS}/${announcementId}?key=${Constants.FIREBASE_KEY}`;
-    const method = 'DELETE';
+    const method = 'delete';
+    const options = {
+      resourceType: 'Announcement',
+    };
 
     try {
-      const response = await AnnouncementsApiComponent(url, method);
+      const response = await GenericApiComponent(url, method, null, options);
 
       if (response) {
         return {success: true, message: 'Announcement deleted successfully'};
@@ -102,7 +90,7 @@ const AnnouncementsService = {
         return {success: false, error: 'Failed to delete announcement'};
       }
     } catch (error) {
-      console.error('Error in Announcement Service.deleteAnnouncement:', error);
+      console.error('Error in AnnouncementsService.deleteAnnouncement:', error);
       return {
         success: false,
         error: error.message || 'An unexpected error occurred.',
